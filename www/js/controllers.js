9720bc068f7731ff4bb55bddfb058153847b85a2
Natanael de Sousa Leite <natanael.leite@serpro.gov.br>
2017-01-18 15:40:47 -0200

    Colocando o pino no mapa indicando a posição atual

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -193,67 +193,20 @@ angular.module('starter')
 
       $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
+      //Wait until the map is loaded
+      google.maps.event.addListenerOnce($scope.map, 'idle', function () {
+
+        var marker = new google.maps.Marker({
+          map: $scope.map,
+          animation: google.maps.Animation.DROP,
+          position: latLng
+        });
+
+      });
+
+
+
     }, function (error) {
       console.log("Could not get location");
     });
   });
-
-// .controller('MapCtrl', function($scope, $ionicLoading, $compile, $log) {
-//       function initialize() {
-//         $log.debug('initialize');
-//         var myLatlng = new google.maps.LatLng(43.07493,-89.381388);
-        
-//         var mapOptions = {
-//           center: myLatlng,
-//           zoom: 16,
-//           mapTypeId: google.maps.MapTypeId.ROADMAP
-//         };
-//         var map = new google.maps.Map(document.getElementById("map"),
-//             mapOptions);
-        
-//         //Marker + infowindow + angularjs compiled ng-click
-//         var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
-//         var compiled = $compile(contentString)($scope);
-
-//         var infowindow = new google.maps.InfoWindow({
-//           content: compiled[0]
-//         });
-
-//         var marker = new google.maps.Marker({
-//           position: myLatlng,
-//           map: map,
-//           title: 'Uluru (Ayers Rock)'
-//         });
-
-//         google.maps.event.addListener(marker, 'click', function() {
-//           infowindow.open(map,marker);
-//         });
-
-//         $scope.map = map;
-//       }
-//       // google.maps.event.addDomListener(window, 'load', initialize);
-//       initialize();
-      
-//       $scope.centerOnMe = function() {
-//         if(!$scope.map) {
-//           return;
-//         }
-
-//         $scope.loading = $ionicLoading.show({
-//           content: 'Getting current location...',
-//           showBackdrop: false
-//         });
-
-//         navigator.geolocation.getCurrentPosition(function(pos) {
-//           $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-//           $scope.loading.hide();
-//         }, function(error) {
-//           alert('Unable to get location: ' + error.message);
-//         });
-//       };
-      
-//       $scope.clickTest = function() {
-//         alert('Example of infowindow with ng-click')
-//       };
-      
-//     });

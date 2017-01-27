fb676467686b852c1024ae2936f771d8007c9053
Natanael de S. Leite <natansl@gmail.com>
2017-01-27 18:41:16 -0200

    Ajustada a imagem do avatar

--- www/js/routes.js
+++ www/js/routes.js
@@ -3,7 +3,7 @@ angular.module('pegapro')
 
 function configRoutes($stateProvider, $urlRouterProvider) {
   // Default:
-  $urlRouterProvider.otherwise('listagem');
+  $urlRouterProvider.otherwise('login');
 
   $stateProvider
     .state('login', {

d91836f076f0e0706a78122079c13458e9ab737c
natansl <natansl@gmail.com>
2017-04-14 14:41:04 -0300

    Adicionado comportamento de infinite scroll na tela de listagem de profissionais

--- www/js/controllers/profissionalListCtrl.js
+++ www/js/controllers/profissionalListCtrl.js
@@ -4,6 +4,8 @@ angular.module('pegapro')
 function profissionalListCtrl($scope, profissionalService, $ionicLoading, $ionicPopup, $log) {
   $log.debug('[profissionalListCtrl] constructor()');
 
+  $scope.listlength = 3;
+
   $scope.show = function () {
     $ionicLoading.show({
       template: '<p>Aguarde...</p><ion-spinner icon="lines"></ion-spinner>'
@@ -26,5 +28,16 @@ function profissionalListCtrl($scope, profissionalService, $ionicLoading, $ionic
   }).finally(function ($ionicLoading) {
     $scope.hide($ionicLoading);
   });
+
+  $scope.loadMore = function(){
+    if (!$scope.listaProfissional){
+      $scope.$broadcast('scroll.infiniteScrollComplete');
+      return;
+    }
+
+    if ($scope.listlength < $scope.listaProfissional.length)
+      $scope.listlength+=1;
+    $scope.$broadcast('scroll.infiniteScrollComplete');
+  }
 
 }
